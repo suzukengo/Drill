bb572ca4facc1074f01d42c5ca10ca1f235a1768
suzukengo <k10212000@gmail.com>
2022-09-09 16:56:15 +0900

    mysqlへの接続をrootでも可能にしました。

--- src/main/java/dao/KaitouDAO.java
+++ src/main/java/dao/KaitouDAO.java
@@ -29,9 +29,11 @@ public class KaitouDAO {
 
 	// データベースのユーザー名 （デフォルトではroot）
 	private final static String USER_NAME = "kengo";
+	//private final static String USER_NAME = "root";
 
 	// データベースのユーザーのパスワード (デフォルトでは設定なし)
 	private final static String PASSWORD = "Suzuki1021#";
+	//private final static String PASSWORD = "";
 	// DockerでMysqlを起動した場合
 	// private final static String PASSWORD = "root";
 
--- src/main/java/dao/ManyKaitouDAO.java
+++ src/main/java/dao/ManyKaitouDAO.java
@@ -31,9 +31,11 @@ public class ManyKaitouDAO {
 
 	// データベースのユーザー名 （デフォルトではroot）
 	private final static String USER_NAME = "kengo";
+	//private final static String USER_NAME = "root";
 
 	// データベースのユーザーのパスワード (デフォルトでは設定なし)
 	private final static String PASSWORD = "Suzuki1021#";
+	//private final static String PASSWORD = "";
 	// DockerでMysqlを起動した場合
 	// private final static String PASSWORD = "root";
 
