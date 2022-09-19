51c2175add271a569fdd6e0bb3849c0b192ee8a9
suzukengo <102952313+suzukengo@users.noreply.github.com>
2022-09-19 12:14:06 +0900

    仮完成

--- src/main/java/dao/RiyoushaDAO.java
+++ src/main/java/dao/RiyoushaDAO.java
@@ -26,13 +26,13 @@ public class RiyoushaDAO {
 	// Mysql5.系
 	// private final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
 	// Mysql8.系
-	private final static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	private final static String DRIVER_NAME = "root";
 
 	// データベースのユーザー名 （デフォルトではroot）
-	private final static String USER_NAME = "root";
+	private final static String USER_NAME = "";
 
 	// データベースのユーザーのパスワード (デフォルトでは設定なし)
-	private final static String PASSWORD = "";
+	private final static String PASSWORD = "Suzuki1021#";
 	// DockerでMysqlを起動した場合
 	// private final static String PASSWORD = "root";
 
